f44932c3848a2b930bae623017b26b343c1db801
Sergio Morales <sergio.morales.external@axa.com>
2019-12-13 13:40:23 +0100

    ✨ Add third page

---
+++ src/pages/page-3.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+
+const thirdPage = () => {
+  return (
+    <Layout>
+      <h1>Welcome to the third page</h1>
+      <p>
+        Velit et aut similique rerum dolor quisquam sunt sed saepe. Eos quia
+        aliquid voluptates quidem beatae voluptas repudiandae et amet. Maxime
+        cupiditate dolor omnis non sit beatae dolorem ut eum. Est et rem cum ab
+        ducimus.
+      </p>
+      <p>
+        Neque id enim itaque nobis qui aut. Dignissimos error sint qui et esse.
+        Aut sed non maxime.
+      </p>
+      <p>
+        Itaque sunt quia mollitia soluta quia voluptas. Molestias sint nihil
+        libero ex et rem quia quo ratione. Natus a sit enim blanditiis officiis.
+        Voluptas quia est omnis.
+      </p>
+      <p>
+        Inventore necessitatibus aut. Et libero fuga sapiente in. Nam fugiat
+        consequuntur deleniti sint sequi magni. Inventore molestiae nisi alias
+        voluptates molestiae.
+      </p>
+      <Link to="/">Return home</Link>
+    </Layout>
+  )
+}
+
+export default thirdPage
